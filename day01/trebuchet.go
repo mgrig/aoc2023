c2ee057b3c322c1c3bd029d71303db9c560b8b15
Marian Grigoras <marian.grigoras@innoq.com>
2023-12-01 13:10:06 +0200

    Move map closer to its usage. Remove printout.

--- day01/trebuchet.go
+++ day01/trebuchet.go
@@ -2,7 +2,6 @@ package day01
 
 import (
 	"aoc2023/common"
-	"fmt"
 	"regexp"
 	"strconv"
 )
@@ -22,6 +21,22 @@ func Part1(lines []string) int {
 	return sum
 }
 
+func Part2(lines []string) int {
+	reFirst, _ := regexp.Compile(`.*?(one|two|three|four|five|six|seven|eight|nine|\d).*`) // lazy first .*
+	reLast, _ := regexp.Compile(`.*(one|two|three|four|five|six|seven|eight|nine|\d).*?`)  // lazy last .*
+	sum := 0
+
+	for _, line := range lines {
+		first := stringFunnyToProper(common.GetOneRegexGroup(reFirst, line))
+		last := stringFunnyToProper(common.GetOneRegexGroup(reLast, line))
+		// fmt.Println(first, last)
+		nr, _ := strconv.Atoi(first + last)
+		sum += nr
+	}
+
+	return sum
+}
+
 var funny2proper = map[string]string{
 	"one":   "1",
 	"two":   "2",
@@ -32,22 +47,6 @@ var funny2proper = map[string]string{
 	"seven": "7",
 	"eight": "8",
 	"nine":  "9",
-}
-
-func Part2(lines []string) int {
-	reFirst, _ := regexp.Compile(`.*?(one|two|three|four|five|six|seven|eight|nine|\d).*`) // lazy first .*
-	reLast, _ := regexp.Compile(`.*(one|two|three|four|five|six|seven|eight|nine|\d).*?`)  // lazy last .*
-	sum := 0
-
-	for _, line := range lines {
-		first := stringFunnyToProper(common.GetOneRegexGroup(reFirst, line))
-		last := stringFunnyToProper(common.GetOneRegexGroup(reLast, line))
-		fmt.Println(first, last)
-		nr, _ := strconv.Atoi(first + last)
-		sum += nr
-	}
-
-	return sum
 }
 
 // "two" -> "2"
